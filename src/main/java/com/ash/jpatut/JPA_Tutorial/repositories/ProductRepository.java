64b5df5a08ef13d42fd263fa61cb05505b85c8c3
ashbbdu <ashishsrivastava7007@gmail.com>
2026-05-01 00:07:44 +0530

    .save

--- src/main/java/com/ash/jpatut/JPA_Tutorial/repositories/ProductRepository.java
+++ src/main/java/com/ash/jpatut/JPA_Tutorial/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.ash.jpatut.JPA_Tutorial.repositories;
 
 import com.ash.jpatut.JPA_Tutorial.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity , Long> {
 }
--- src/test/java/com/ash/jpatut/JPA_Tutorial/JpaTutorialApplicationTests.java
+++ src/test/java/com/ash/jpatut/JPA_Tutorial/JpaTutorialApplicationTests.java
@@ -1,13 +1,29 @@
 package com.ash.jpatut.JPA_Tutorial;
 
+import com.ash.jpatut.JPA_Tutorial.entities.ProductEntity;
+import com.ash.jpatut.JPA_Tutorial.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
 
 @SpringBootTest
 class JpaTutorialApplicationTests {
 
+	@Autowired
+	public ProductRepository productRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+
+	@Test
+	void testRepository () {
+		ProductEntity product = ProductEntity.builder().price(new BigDecimal(10)).title("New Title")
+				.category("juice").sku("SKU-0909").build();
+		productRepository.save(product);
 	}
 
 }
