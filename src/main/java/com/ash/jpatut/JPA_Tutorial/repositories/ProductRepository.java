eccd0cc432b883f970ba9b746cb746d255c97cfb
ashbbdu <ashishsrivastava7007@gmail.com>
2026-05-01 12:07:42 +0530

    custom queries

--- src/main/java/com/ash/jpatut/JPA_Tutorial/repositories/ProductRepository.java
+++ src/main/java/com/ash/jpatut/JPA_Tutorial/repositories/ProductRepository.java
@@ -4,6 +4,17 @@ import com.ash.jpatut.JPA_Tutorial.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity , Long> {
+
+//    custom queries
+    public List<ProductEntity> findByTitle(String title);
+
+    public List<ProductEntity> findByCreatedAtBetween(LocalDateTime starDate , LocalDateTime endDate);
+
+//    will return first two result
+    public List<ProductEntity> findFirst2ByCreatedAtBetween(LocalDateTime starDate , LocalDateTime endDate);
 }
--- src/test/java/com/ash/jpatut/JPA_Tutorial/JpaTutorialApplicationTests.java
+++ src/test/java/com/ash/jpatut/JPA_Tutorial/JpaTutorialApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootTest
@@ -24,13 +25,40 @@ class JpaTutorialApplicationTests {
 	void testRepository () {
 		ProductEntity product = ProductEntity.builder().price(new BigDecimal(101111)).title("New Title")
 				.category("juice").quantity(10).sku("SKU-0211219019111").build();
-		ProductEntity savedProduct = productRepository.save(product);
-		System.out.println(savedProduct);
+//		ProductEntity savedProduct = productRepository.save(product);
+//		System.out.println(savedProduct);
 	}
 
 	@Test
 	void getRepository () {
 		List<ProductEntity> products = productRepository.findAll();
+//		System.out.println(products);
+	}
+
+
+//	custom queries
+	@Test
+	void getRepositoryCustom () {
+		List<ProductEntity> products = productRepository.findByTitle("Parle G");
+//		System.out.println(products);
+	}
+
+	@Test
+	void getRepositoryByDates () {
+		List<ProductEntity> products = productRepository.
+				findByCreatedAtBetween(LocalDateTime.of(2026, 5, 1, 0, 0 , 0),
+				LocalDateTime.of(2026, 5, 2, 0 ,0, 0));
+
+		System.out.println(products);
+	}
+
+//	will return first two First2
+	@Test
+	void getRepositoryByDatesFirstTwo () {
+		List<ProductEntity> products = productRepository.
+				findFirst2ByCreatedAtBetween(LocalDateTime.of(2026, 5, 1, 0, 0 , 0),
+						LocalDateTime.of(2026, 5, 2, 0 ,0, 0));
+
 		System.out.println(products);
 	}
 
